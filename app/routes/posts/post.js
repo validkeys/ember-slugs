317c4ce8a7f8fc1ca32f64319d4f02ec055da871
Kyle Davis <validkeys@gmail.com>
2016-08-11 09:03:01 -0400

    added some notes

--- app/routes/posts/post.js
+++ app/routes/posts/post.js
@@ -2,6 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
+    // The serialize method below is used
+    // by the link-to helper as well as transitions. But if
+    // you deep link to this page, there is no transition and
+    // therefore the serialize method is not called. So here
+    // we are just checking to see if the params has a post_id
+    // and if not, we just grab the id out of the slug.
+    // the below regex assumes a slug format of [id]-[dasherized-title]
     if (!params.post_id) {
       params.post_id = params.post_slug.match(/[^-]*/)[0];
     }
